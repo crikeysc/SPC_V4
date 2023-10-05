e4ceafb067a9289897ef1ccb6980005c9102975f
SPC <sclark@nhp.com.au>
2023-10-05 17:22:01 +1100

    Optix Panle 12' Included

--- ProjectFiles/NetSolution/AlarmGridLogic.cs
+++ ProjectFiles/NetSolution/AlarmGridLogic.cs
@@ -1,35 +1,37 @@
 #region Using directives
-using FTOptix.NetLogic;
-using FTOptix.UI;
+using System;
 using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.HMIProject;
+using FTOptix.NetLogic;
+using FTOptix.NativeUI;
+using FTOptix.UI;
+using FTOptix.Alarm;
+using FTOptix.CoreBase;
+using FTOptix.DataLogger;
+using FTOptix.EventLogger;
+using FTOptix.SQLiteStore;
+using FTOptix.Store;
+using FTOptix.Report;
+using FTOptix.Modbus;
 using FTOptix.RAEtherNetIP;
+using FTOptix.Retentivity;
+using FTOptix.CommunicationDriver;
+using FTOptix.SerialPort;
 using FTOptix.System;
+using FTOptix.UI;
+using FTOptix.Core;
 #endregion
 
-public class AlarmGridLogic : BaseNetLogic {
-    public override void Start() {
-        alarmsDataGridModel = Owner.Get<DataGrid>("AlarmsDataGrid").GetVariable("Model");
-
-        var currentSession = LogicObject.Context.Sessions.CurrentSessionInfo;
-        actualLanguagesVariable = currentSession.SessionObject.Get<IUAVariable>("ActualLanguage");
-        actualLanguagesVariable.VariableChange += OnSessionActualLanguagesChange;
+public class AlarmGridLogic : BaseNetLogic
+{
+    public override void Start()
+    {
+        // Insert code to be executed when the user-defined logic is started
     }
 
-    public override void Stop() {
-        actualLanguagesVariable.VariableChange -= OnSessionActualLanguagesChange;
+    public override void Stop()
+    {
+        // Insert code to be executed when the user-defined logic is stopped
     }
-
-    public void OnSessionActualLanguagesChange(object sender, VariableChangeEventArgs e) {
-        var dynamicLink = alarmsDataGridModel.GetVariable("DynamicLink");
-        if (dynamicLink == null)
-            return;
-
-        // Restart the data bind on the data grid model variable to refresh data
-        string dynamicLinkValue = dynamicLink.Value;
-        dynamicLink.Value = string.Empty;
-        dynamicLink.Value = dynamicLinkValue;
-    }
-
-    private IUAVariable alarmsDataGridModel;
-    private IUAVariable actualLanguagesVariable;
 }
