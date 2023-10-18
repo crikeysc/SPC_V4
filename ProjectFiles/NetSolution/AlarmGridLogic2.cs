d3dd1398198251c4720dceec60b70be081018b93
sclark <sclark@nhp.com.au>
2023-10-18 17:22:49 +1100

    Motor Popup

---
+++ ProjectFiles/NetSolution/AlarmGridLogic2.cs
@@ -0,0 +1,38 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.HMIProject;
+using FTOptix.NetLogic;
+using FTOptix.NativeUI;
+using FTOptix.UI;
+using FTOptix.WebUI;
+using FTOptix.CoreBase;
+using FTOptix.Alarm;
+using FTOptix.DataLogger;
+using FTOptix.EventLogger;
+using FTOptix.SQLiteStore;
+using FTOptix.Store;
+using FTOptix.Report;
+using FTOptix.Modbus;
+using FTOptix.RAEtherNetIP;
+using FTOptix.Retentivity;
+using FTOptix.CommunicationDriver;
+using FTOptix.System;
+using FTOptix.SerialPort;
+using FTOptix.UI;
+using FTOptix.Core;
+#endregion
+
+public class AlarmGridLogic2 : BaseNetLogic
+{
+    public override void Start()
+    {
+        // Insert code to be executed when the user-defined logic is started
+    }
+
+    public override void Stop()
+    {
+        // Insert code to be executed when the user-defined logic is stopped
+    }
+}
